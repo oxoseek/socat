a1e71c00529ddca6102bb32cbb6075c64b5dcd9a
oxo seek <loox@duck.com>
2026-01-14 10:46:23 +0800

    Update ss-2022.sh

--- ss-2022.sh
+++ ss-2022.sh
@@ -163,7 +163,7 @@ get_latest_version() {
 
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m" && Yellow_font_prefix="\033[0;33m"
 Info="${Green_font_prefix}[信息]${Font_color_suffix}"
-Error="${Red_font_prefix}[错误]${Font_color_suffix}"
+错误="${Red_font_prefix}[错误]${Font_color_suffix}"
 Tip="${Yellow_font_prefix}[注意]${Font_color_suffix}"
 
 check_installed_status() {
@@ -1137,7 +1137,7 @@ Start_Menu() {
     echo -e "${GREEN}          SS - 2022 管理脚本 ${RESET}"
     echo -e "${GREEN}============================================${RESET}"
     echo -e "${GREEN}            作者: jinqian${RESET}"
-    echo -e "${GREEN}       网站：${RESET}"
+    echo -e "${GREEN}        网站：https://jinqians.com${RESET}"
     echo -e "${GREEN}============================================${RESET}"
         echo && echo -e "  
  ${Green_font_prefix}0。${Font_color_suffix} 更新脚本
